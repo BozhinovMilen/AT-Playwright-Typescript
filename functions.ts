c82879bacee3d31d1e22528f195ad66b9cd1d497
Milen Bozhinov <milen.bojinov@pragmatic.bg>
2025-02-27 23:00:05 +0200

    update-arrays-functions

--- functions.ts
+++ functions.ts
@@ -20,12 +20,25 @@ function greeting(): void {
 
 greeting();
 
+// Optional parameters
 function greetingNameAgeOptionalPrameters(name: string, age?: number): string {
+    if (age) {
+        return `Hello, ${name}, You are ${age} years old!`;
+    } else {
+        return `Hello, ${name}`;
+    }
+}
+console.log(greetingNameAgeOptionalPrameters("Alice"));
+console.log(greetingNameAgeOptionalPrameters("Alice", 25));
+
+
+function greetingNameAgeOptionalPrametersTernary(name: string, age?: number): string {
     return `Hello, ${name}, ${age ? `You are ${age} years old!` : ""} `;
 }
-console.log(greetingNameAgeOptionalPrameters("Alice"))
-console.log(greetingNameAgeOptionalPrameters("Alice", 25))
+console.log(greetingNameAgeOptionalPrametersTernary("Alice"));
+console.log(greetingNameAgeOptionalPrametersTernary("Alice", 25));
 
+// Default parameters
 function  subtractNumbers(firstNum: number = 10, secondNum: number = 5): number {
     return firstNum - secondNum;
 }
@@ -33,3 +46,11 @@ function  subtractNumbers(firstNum: number = 10, secondNum: number = 5): number 
 console.log(subtractNumbers());
 console.log(subtractNumbers(20));
 console.log(subtractNumbers(20, 7));
+
+// Arrow functions
+const square = (x: number): number => x * x;
+// let square = (x: number): number => x * x;
+square(5)
+let squareResult: number = square(3 );
+console.log(square(3));
+console.log(square(5));
